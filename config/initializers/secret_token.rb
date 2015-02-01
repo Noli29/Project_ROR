1acac84413a104b7557b61900743e46f718f97d5
Noli29 <noli.pogorelova@gmail.com>
2015-02-01 23:26:21 +0200

    Create user and admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lalala::Application.config.secret_token = '446c516afcc8681189346bc9aad44700bc6f8523227ecbbaca398afb1a096d571012c19b1af46c55d3508f0406b619b75602b4fd83c18ebaf8375ca76af25859'

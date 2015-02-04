90bf57bfae14434ed5d5edbc4c4d3fe762992c72
Noli29 <noli.pogorelova@gmail.com>
2015-02-05 01:23:25 +0200

    Views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,6 @@ class Ability
 
     user ||= User.new
 
-    # Define User abilities
-    if user.is? :admin
-      can :manage, Article
-    else
-      can :read, Article
-    end
-
-
     if user.is_a?(User)
       can [:create, :update,:show], [User, Profile]
     elsif user.is_a?(Admin)
